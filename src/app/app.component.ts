ec7acbd333566aff6e95fbfc8377c780927b764c
David Allen <davetvmusic@hotmail.com>
2020-08-26 20:02:57 +1000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, throwError } from 'rxjs';
+import { concatMap, delay, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rxjs-playground';
+export class AppComponent implements OnInit {
+
+  public obs1$ = of('Dave').pipe(delay(4000));
+  // public obs1$ = throwError('This is an error!');
+
+  public ngOnInit(): void {
+    this.obs1$.pipe(mergeMap(val => of(`${val} Allen`)))
+      .subscribe(el => console.log(el)
+      )
+  }
 }
