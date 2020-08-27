ad65edb1383cca5f8526765913e3ee5c79410bbd
David Allen <davetvmusic@hotmail.com>
2020-08-27 19:40:00 +1000

    add switchmap & concatMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { of, throwError } from 'rxjs';
-import { concatMap, delay, mergeMap } from 'rxjs/operators';
+import { of, throwError, fromEvent, interval } from 'rxjs';
+import { concatMap, delay, mergeMap, switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,36 @@ import { concatMap, delay, mergeMap } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
 
+  public counter: number = 0;
+  public searchControl: FormControl = new FormControl();
+
   public obs1$ = of('Dave').pipe(delay(4000));
   // public obs1$ = throwError('This is an error!');
 
   public ngOnInit(): void {
-    this.obs1$.pipe(mergeMap(val => of(`${val} Allen`)))
-      .subscribe(el => console.log(el)
-      )
+    // MERGEMAP / CONCATMAP (main diff - concatmap sequence of execution is guaranteed)
+    // mergemap executes in parallel (async) (merge 2 obs into 1)
+    // this.obs1$.pipe(mergeMap(val => of(`${val} Allen`)))
+    //   .subscribe(el => console.log(el)
+    //   )
+    
+    // swap concatMap & mergeMap
+    of(1, 2, 3).pipe(
+        concatMap(num => of(`the num is ${num}`).pipe(delay(2000)))
+      ).subscribe(console.log);
+    
+
+    // SWITCHMAP cancel previous inner observable & re-subscribe (search)
+    // fromEvent(document, 'click')
+    //   .pipe(switchMap(() => interval(700)))
+    //   .subscribe(el => this.counter = el);
+
+    // this.searchControl.valueChanges.pipe(
+    //   debounceTime(1000),
+    //   distinctUntilChanged(),
+    //   switchMap(() => interval(500))
+    // )
+    // .subscribe(val => this.counter = val);
+
   }
 }
